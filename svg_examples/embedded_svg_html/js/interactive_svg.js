7254ae58de2fb62fab8f29c9c60e6dd7323dfb5f
AntonyGarcia <antony.garcia.gonzalez@gmail.com>
2018-01-02 21:13:09 -0500

    Modificada la estructura interna del repositorio

--- svg_examples/embedded_svg_html/js/interactive_svg.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $("#svg_container").load('images/interactive.svg', function (response) {
-        if (!response) {
-            $(this).html('<strong>Error cargando el SVG</strong>');
-        }
-    });    
-});
