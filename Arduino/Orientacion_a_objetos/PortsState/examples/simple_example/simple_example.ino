6e4a181b8f500a69967d5c63fd24f1f4de8eaa48
Antony García <antony.garcia.gonzalez@gmail.com>
2018-04-07 17:26:20 -0400

    Archivos de ejemplo

---
+++ Arduino/Orientacion_a_objetos/PortsState/examples/simple_example/simple_example.ino
@@ -0,0 +1,28 @@
+#include <PortsState.h>
+PortsState port("04/07/2018", 10, 11, 12, 13, 14, 15);
+
+void setup() {
+Serial.begin(9600);
+while(!Serial);
+Serial.println("Impresion de datos almacenados");
+Serial.print("Fecha: ");
+Serial.println(port.getDate());
+Serial.print("Valor del sensor 0: ");
+Serial.println(port.getSensor0());
+Serial.print("Valor del sensor 1: ");
+Serial.println(port.getSensor1());
+Serial.print("Valor del sensor 2: ");
+Serial.println(port.getSensor2());
+Serial.print("Valor del sensor 3: ");
+Serial.println(port.getSensor3());
+Serial.print("Valor del sensor 4: ");
+Serial.println(port.getSensor4());
+Serial.print("Valor del sensor 5: ");
+Serial.println(port.getSensor5());
+Serial.print("Promedio de los valores de los sensores: ");
+Serial.println(port.getPortsAverage());
+}
+
+void loop() {
+
+}
