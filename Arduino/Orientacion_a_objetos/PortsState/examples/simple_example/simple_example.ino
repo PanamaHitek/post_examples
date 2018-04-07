ad2c4873aee184932056552c626638ac6a3dcbe6
Antony García <antony.garcia.gonzalez@gmail.com>
2018-04-07 17:29:29 -0400

    cambios a archivo de ejemplo

--- Arduino/Orientacion_a_objetos/PortsState/examples/simple_example/simple_example.ino
+++ Arduino/Orientacion_a_objetos/PortsState/examples/simple_example/simple_example.ino
@@ -1,9 +1,10 @@
 #include <PortsState.h>
+//Creacion del objeto de la clase PortsState
 PortsState port("04/07/2018", 10, 11, 12, 13, 14, 15);
 
 void setup() {
+  
 Serial.begin(9600);
-while(!Serial);
 Serial.println("Impresion de datos almacenados");
 Serial.print("Fecha: ");
 Serial.println(port.getDate());
@@ -21,6 +22,7 @@ Serial.print("Valor del sensor 5: ");
 Serial.println(port.getSensor5());
 Serial.print("Promedio de los valores de los sensores: ");
 Serial.println(port.getPortsAverage());
+  
 }
 
 void loop() {
